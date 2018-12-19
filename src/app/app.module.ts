f734edfc2ed0c86947b5cd20a123eb26247c30b5
Sandeep Reddy <bsreddy0600@gmail.com>
2018-12-19 05:07:42 -0700

    Added Sample component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { IndexComponentComponent } from './index-component/index-component.component';
+import { SampleComponent } from './sample/sample.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponentComponent,
+    SampleComponent
   ],
   imports: [
     BrowserModule
