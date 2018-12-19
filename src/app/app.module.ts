5180bc0ce31f0a527e2cf562ada8fa47407acae6
Sandeep Reddy <bsreddy0600@gmail.com>
2018-12-19 05:20:41 -0700

    Added Experience component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { IndexComponentComponent } from './index-component/index-component.component';
 import { SampleComponent } from './sample/sample.component';
+import { ExperienceComponent } from './experience/experience.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     IndexComponentComponent,
-    SampleComponent
+    SampleComponent,
+    ExperienceComponent
   ],
   imports: [
     BrowserModule
